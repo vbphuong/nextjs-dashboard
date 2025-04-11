546177c656b878ec2748db384fcb7508d7a3be0f
vbphuong <vbphuong012@gmail.com>
2025-04-11 06:04:47 -0700

    second

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-    hooks: {
-      readPackageJson(pkg) {
-        if (pkg.name === 'bcrypt' || pkg.name === 'sharp') {
-          pkg.scripts = pkg.scripts || {};
-          pkg.scripts.preinstall = 'node scripts/preinstall.js || true'; // Or similar build command
-        }
-        return pkg;
-      },
+  hooks: {
+    readPackageJson(pkg) {
+      if (pkg.name === 'bcrypt') {
+        pkg.scripts = pkg.scripts || {};
+        pkg.scripts.preinstall = 'pnpm rebuild bcrypt || true';
+      }
+      return pkg;
     },
-  }; 
+  },
+};
