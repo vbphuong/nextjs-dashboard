48435d0475afb9e6ed80cd8d6f121105a08aaedb
vbphuong <vbphuong012@gmail.com>
2025-06-02 08:48:03 -0700

    init

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -20,6 +20,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Products', href: '/dashboard/products', icon: DocumentDuplicateIcon },
 ];
 
 export default function NavLinks() {
