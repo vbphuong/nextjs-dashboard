a3432efe1e5cf4445495ac842b1a3e6f81abaea4
vbphuong <vbphuong012@gmail.com>
2025-06-12 21:56:15 -0700

    doimaimotnguoi

--- app/ui/products/feedback-chart.tsx
+++ app/ui/products/feedback-chart.tsx
@@ -160,7 +160,7 @@ export function FeedbackChart() {
             height={250}
             width={chartWidth || 400} // Fallback width if chartWidth is 0
             data={filteredData}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            margin={{ top: 10, right: 0, left: 0, bottom: 0 }}
           >
             <defs>
               <linearGradient id="fillProvider" x1="0" y1="0" x2="0" y2="1">
