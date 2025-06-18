3ea171667c44c6993f613cb5be812088ec39987a
vbphuong <vbphuong012@gmail.com>
2025-06-17 23:33:35 -0700

    doimaimotnguoi

--- app/ui/market/CompetitiveProductsMap.tsx
+++ app/ui/market/CompetitiveProductsMap.tsx
@@ -19,7 +19,7 @@ export function CompetitiveProductsMap() {
   return (
     <div className="bg-black p-4 rounded-lg shadow-lg">
       <h2 className="text-white text-2xl font-bold mb-4">
-        Highly Competitive Products Map
+        Highly Competitive Products Map 
       </h2>
       <p className="text-gray-300 mb-4">
         Analyze products with many sellers in the same category to understand competitive pressures. If any scale has high density of some types of goods, suggestion for changing location or products is needed.
